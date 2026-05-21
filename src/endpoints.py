80bbf38dd1f2edc897a76ac53cf4e9f7461f69cf
Denis Chudnov PC <denis.chudnov.97@gmail.com>
2026-05-21 12:57:23 +0200

    Add endpoints file. Implement couple of tests for product search endpoints

---
+++ src/endpoints.py
@@ -0,0 +1,9 @@
+PRODUCT_LIST = '/productsList'
+BRAND_LIST = '/brandsList'
+PRODUCT_SEARCH = '/searchProduct'
+VERIFY_LOGIN = '/verifyLogin'
+CREATE_ACCOUNT = '/createAccount'
+DELETE_ACCOUNT = '/deleteAccount'
+UPDATE_ACCOUNT = '/updateAccount'
+GET_USER_ACCOUNT_BY_EMAIL = '/getUserDetailByEmail'
+
--- tests/api_tests/brand.py
+++ tests/api_tests/brand.py
@@ -1,20 +1,24 @@
 import allure
 
+from src.endpoints import BRAND_LIST
+
+
 @allure.suite('brand')
 
 @allure.title('Get all brands list')
 def test_get_all_brands_list(api_client):
-    response = api_client.get('brandsList')
+    response = api_client.get(BRAND_LIST)
     body = response.json()
     with allure.step('Checking response code'):
         assert response.status_code == 200
 
     with allure.step('Checking of response content'):
-        assert (body['brands'] is not None)
+        assert (len(body['brands']) >= 0)
 
 @allure.title('Put to all brands list')
+@allure.description('Test with expectation of response code 200, in case, when this is an appropriate way in team')
 def test_put_to_all_brands_list(api_client):
-    response = api_client.put('brandsList')
+    response = api_client.put(BRAND_LIST)
     body = response.json()
 
     with allure.step('Checking response code'):

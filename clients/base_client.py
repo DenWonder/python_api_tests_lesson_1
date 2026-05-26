882d2555297aed157fcf303bcf930a77ab97da2a
Denis Chudnov PC <denis.chudnov.97@gmail.com>
2026-05-26 20:29:16 +0200

    Add allure attachments on sending requests

--- clients/base_client.py
+++ clients/base_client.py
@@ -1,4 +1,7 @@
+import json
+
 import allure
+from allure_commons.types import AttachmentType
 
 
 class BaseClient:
@@ -22,26 +25,44 @@ class BaseClient:
         if payload is None:
             payload = {}
         with allure.step(f'sending POST request to {url}'):
-            return self.session.post(self.base_url + url, data=payload, headers=headers)
+            allure.attach(body=json.dumps(payload.json(), indent=4, ensure_ascii=True), name='Request payload', attachment_type=allure.attachment_type.JSON)
+            response = self.session.post(self.base_url + url, data=payload, headers=headers)
+            allure.attach(body=json.dumps(response.json(), indent=4, ensure_ascii=True), name='Response', attachment_type=AttachmentType.JSON, extension="json")
+            return response
 
     def delete(self, url, payload=None, headers=None):
         if payload is None:
             payload = {}
         with allure.step(f'sending DELETE request to {url}'):
-            return self.session.delete(self.base_url + url, data=payload, headers=headers)
+            allure.attach(body=json.dumps(payload.json(), indent=4, ensure_ascii=True), name='Request payload', attachment_type=allure.attachment_type.JSON)
+            response = self.session.delete(self.base_url + url, data=payload, headers=headers)
+            allure.attach(body=json.dumps(response.json(), indent=4, ensure_ascii=True), name='Response',
+                      attachment_type=AttachmentType.JSON, extension="json")
+            return response
 
     def patch(self, url, payload=None, headers=None):
         if payload is None:
             payload = {}
         with allure.step(f'sending PATCH request to {url}'):
-            return self.session.patch(self.base_url + url, data=payload, headers=headers)
+            allure.attach(body=json.dumps(payload.json(), indent=4, ensure_ascii=True), name='Request payload', attachment_type=allure.attachment_type.JSON)
+            response = self.session.patch(self.base_url + url, data=payload, headers=headers)
+            allure.attach(body=json.dumps(response.json(), indent=4, ensure_ascii=True), name='Response',
+                          attachment_type=AttachmentType.JSON, extension="json")
+            return response
 
     def get(self, url, headers=None):
         with allure.step(f'sending GET request to {url}'):
-            return self.session.get(self.base_url + url, headers=headers)
+            response = self.session.get(self.base_url + url, headers=headers)
+            allure.attach(body=json.dumps(response.json(), indent=4, ensure_ascii=True), name='Response',
+                          attachment_type=AttachmentType.JSON, extension="json")
+            return response
 
     def put(self, url, payload=None, headers=None):
         if payload is None:
             payload = {}
         with allure.step(f'sending PUT request to {url}'):
-            return self.session.put(self.base_url + url, data=payload, headers=headers)
+            allure.attach(body=json.dumps(payload.json(), indent=4, ensure_ascii=True), name='Request payload', attachment_type=allure.attachment_type.JSON)
+            response = self.session.put(self.base_url + url, data=payload, headers=headers)
+            allure.attach(body=json.dumps(response.json(), indent=4, ensure_ascii=True), name='Response',
+                          attachment_type=AttachmentType.JSON, extension="json")
+            return response
